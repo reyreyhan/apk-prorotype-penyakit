3a8d88c54df5316a192558d676c025f4896516b3
severalpuns <newrey9227@gmail.com>
2018-02-01 22:41:01 +0700

    prororype

---
+++ app/src/main/java/com/example/reyhan/e_healty/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.reyhan.e_healty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    public WebView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.wv);
+        view.getSettings().setJavaScriptEnabled(true);
+
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("file:///android_asset/login/login.html");
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //ketika disentuh tombol back
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+            view.goBack(); //method goback(),untuk kembali ke halaman sebelumnya
+            return true;
+        }
+        // Jika tidak ada halaman yang pernah dibuka
+        // maka akan keluar dari activity (tutup aplikasi)
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
